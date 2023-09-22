a37c6546b9c2005573f7477dda65a57c175ab900
LauraDa999 <141860731+LauraDa999@users.noreply.github.com>
2023-09-22 18:07:08 -0500

    Update Punto 1.py

--- Punto 1.py
+++ Punto 1.py
@@ -1,9 +1,8 @@
-# Leer tres números reales desde el usuario
+
 numero1 = float(input("Ingrese el primer número: "))
 numero2 = float(input("Ingrese el segundo número: "))
 numero3 = float(input("Ingrese el tercer número: "))
 
-# Determinar cuál es el número mayor
 if numero1 >= numero2 and numero1 >= numero3:
     mayor = numero1
 elif numero2 >= numero1 and numero2 >= numero3:
@@ -11,5 +10,4 @@ elif numero2 >= numero1 and numero2 >= numero3:
 else:
     mayor = numero3
 
-# Mostrar el resultado
 print(f"El número mayor es: {mayor}")
