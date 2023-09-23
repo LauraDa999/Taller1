3cf043b24dc483d023f8bb7e2d1c999e0d569731
LauraDa999 <141860731+LauraDa999@users.noreply.github.com>
2023-09-22 20:45:35 -0500

    Add files via upload

---
+++ Punto#06.py
@@ -0,0 +1,7 @@
+c = input("Ingresa una letra cualquiera: ")
+vocales =  ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+if c in vocales:
+    print("Es una vocal") 
+else:     
+    print("Es una consonante")
+    
